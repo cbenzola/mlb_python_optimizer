402b71afcfb6632add936500adb9242034577fda
Chris Benzola <56400496+cbenzola@users.noreply.github.com>
2020-08-08 09:45:27 -0400

    Update dailyscrape.py

--- dailyscrape.py
+++ dailyscrape.py
@@ -6,9 +6,7 @@ import xlrd
 
 
 def simple_get(url):
-    """
-    Attempts to get the content at 'url' by making an HTTP GET request.
-    If the content-type of response is HTML/XML, return the text content, otherwise return None."""
+    
     try:
         with closing(get(url, stream = True)) as resp:
             if is_good_response(resp):
@@ -19,7 +17,7 @@ def simple_get(url):
         log_error('Error during requests to {0} : {1}'.format(url, str(e)))
         return None
 def is_good_response(resp):
-    """ Returns True if the response seems to be HTML, False otherwise."""
+   
     content_type = resp.headers['Content-Type'].lower()
     return (resp.status_code == 200
             and content_type is not None
@@ -28,11 +26,7 @@ def log_error(e):
     print(e)
 
 def create_players(player_list):
-    """
-    Loops through every MLB player, and for those who are playing on the given slate,
-    creates a Player object to represent the player, and adds that to player_list
-    :return: None
-    """
+    
     for i in range(roto_info.nrows):
         row = roto_info.row_values(i)
         player_list.append(Player(row[0],row[1], row[3],row[2]))
